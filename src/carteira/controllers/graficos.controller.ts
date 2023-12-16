c8d7a1483da478a699aaf36875f2e906cb351ae9
Luiz Alencar <lf.dacol@gmail.com>
2023-12-16 16:30:35 -0300

    feat: cria endpoint para criacao do graficos de proventos

--- src/carteira/controllers/graficos.controller.ts
+++ src/carteira/controllers/graficos.controller.ts
@@ -9,10 +9,16 @@ import { CarteiraRendaVariavelDto } from '../dto/carteira-renda-variavel.dto';
 import { CarteiraRendaFixaDto } from '../dto/carteira-renda-fixa.dto';
 import { TipoAtivo } from 'src/enums/tipo-ativo.enum';
 import { ComposicaoChartDto } from '../dto/composicao-chart.dto';
+import { ProventosService } from 'src/renda-variavel/proventos.service';
+import { ProventosChartDto } from '../dto/proventos-chart.dto';
+import { Provento } from 'src/renda-variavel/entities/provento.entity';
 
 @Controller('v1/graficos')
 export class GraficosController {
-  constructor(private readonly carteiraService: CarteiraService) {}
+  constructor(
+    private readonly carteiraService: CarteiraService,
+    private readonly proventosService: ProventosService,
+  ) {}
 
   @UseInterceptors(ClassSerializerInterceptor)
   @Get('composicao')
@@ -51,5 +57,51 @@ export class GraficosController {
     );
 
     return composicaoDto;
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('proventos')
+  async getProventos(): Promise<ProventosChartDto[]> {
+    const proventos = await this.proventosService.findAll();
+
+    const initialValue = [];
+    const startDate = new Date('2021-08-01T00:00:00.000Z');
+    const endDate = new Date();
+    while (startDate < endDate) {
+      initialValue.push({
+        data: new Date(startDate),
+        fii: 0,
+        acao: 0,
+        bdr: 0,
+        carteira: 0,
+      } as ProventosChartDto);
+      startDate.setMonth(startDate.getMonth() + 1);
+    }
+
+    const proventosDto = proventos.reduce(
+      (chartDto: ProventosChartDto[], item: Provento) => {
+        const proventoMes = chartDto.find(
+          (c) =>
+            c.data.getUTCMonth() === item.dataPagamento.getUTCMonth() &&
+            c.data.getUTCFullYear() === item.dataPagamento.getUTCFullYear(),
+        );
+
+        if (!proventoMes) return chartDto;
+
+        proventoMes.carteira += item.valorTotal;
+        if (item.ativo.tipo === TipoAtivo.ACAO) {
+          proventoMes.acao += item.valorTotal;
+        } else if (item.ativo.tipo === TipoAtivo.FII) {
+          proventoMes.fii += item.valorTotal;
+        } else if (item.ativo.tipo === TipoAtivo.BDR) {
+          proventoMes.bdr += item.valorTotal;
+        }
+
+        return chartDto;
+      },
+      initialValue,
+    );
+
+    return proventosDto;
   }
 }
---
+++ src/carteira/dto/proventos-chart.dto.ts
@@ -0,0 +1,7 @@
+export class ProventosChartDto {
+  data: Date;
+  fii: number;
+  acao: number;
+  bdr: number;
+  carteira: number;
+}
