67e789c22c9acf23f29117e69fef62cd83392900
Luiz Alencar <lf.dacol@gmail.com>
2025-07-05 13:54:58 -0300

    feat: adiciona modulo de cripto no modulo de carteira

--- src/carteira/carteira.module.ts
+++ src/carteira/carteira.module.ts
@@ -4,9 +4,10 @@ import { CarteiraController } from './controllers/carteira.controller';
 import { RendaFixaModule } from '../renda-fixa/renda-fixa.module';
 import { GraficosController } from './controllers/graficos.controller';
 import { RendaVariavelModule } from '../renda-variavel/renda-variavel.module';
+import { CriptomoedasModule } from '../criptomoedas/criptomoedas.module';
 
 @Module({
-  imports: [RendaVariavelModule, RendaFixaModule],
+  imports: [RendaVariavelModule, RendaFixaModule, CriptomoedasModule],
   controllers: [CarteiraController, GraficosController],
   providers: [CarteiraService],
 })
